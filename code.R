c99ff89079efc4b5f457411231261ede01f58dd7
M.Durga Prasad <mdp0999@gmail.com>
2017-04-01 12:35:19 +0530

    Update code.R

--- code.R
+++ code.R
@@ -1,38 +1,46 @@
-
-#R offers several packages to build n-grams.It was difficult to find an efficient method to construct the n-grams
-#adjusted to the computing resources available. Several packages were tested and discarted, included RWeka, tm, openNLP, ngram,datasets,shiny and knitr,stringi,tm.
-
 rm(list=ls(all=T))
 libs <- c("knitr","stringi","tm","RWeka")
 lapply(libs,require,character.only=TRUE )
 library(knitr)
 opts_chunk$set(cache=TRUE,warning=FALSE)
 
+freqs1<-scan(file=file.choose(), what="char", sep="\n", encoding="UTF-8")
+freqs2<-scan(file=file.choose(), what="char", sep="\n", encoding="UTF-8")
+freqs3<-scan(file=file.choose(), what="char", sep="\n", encoding="UTF-8")
+Media files general statistics:
 
-freqs<-scan(file=file.choose(), what="char", sep="\n", encoding="UTF-8")
 
-
-#Media files general statistics:
 #Lines
-freqsLines <- length(freqs)
-
+freqs1Lines <- length(freqs1)
+freqs2Lines <- length(freqs2)
+freqs3Lines   <- length(freqs3)
 
 
 #Characters
-freqsNchar   <- sum(nchar(freqs,type="chars"))
+freqs1Nchar   <- sum(nchar(freqs1,type="chars"))
+freqs2Nchar    <- sum(nchar(freqs2,type="chars"))
+freqs3Nchar <- sum(nchar(freqs3,type="chars"))
 
 #words statistics :
-sum(stri_count_words(freqs))
-
+sum(stri_count_words(freqs1))
+sum(stri_count_words(freqs2))
+sum(stri_count_words(freqs3))
 
 #Other statistics
 #Summary statistics :
-stri_stats_general(freqs)
+stri_stats_general(freqs1)
+stri_stats_general(freqs2)
+stri_stats_general(freqs3)
 
+#Due to the dataset size and the processing time required,and my pc limtations i will only sample the freqs2 documents assuming that the sample size allowed by my pc is representative of the freqs2 raw data.
+#The exloratory analysis woud be similar for the other medias.
+set.seed(100)
+sample.size=300
+mediaSample  <- freqs2[sample(c(1:length(freqs2)),sample.size,replace=FALSE)]
 # preview the sample before cleaning
-head(freqs)
-tail(freqs)
-#(text cleaningPrior to corpus Tokenization and for an adequate text mining data need to be cleaned in order to extract significant features.
+head(freqs2)
+tail(freqs2)
+#text cleaningPrior to corpus Tokenization and for an adequate text mining data need to be cleaned in order to extract significant features.
 #The cleaning function cleans as follow:
 #remove no alphanumeric
 #remove stop word
@@ -41,12 +49,14 @@ tail(freqs)
 #remove no english words
 #remove numbers
 #strip white spaces
-#Not all of them are necessary i will get back to this in next weeks.)
-doc.vec <- VectorSource(freqs)
+#Not all of them are necessary i will get back to this in next weeks.
+
+doc.vec <- VectorSource(freqs2)
 mycorpus <- Corpus(doc.vec)
 summary(mycorpus)
 mycorpus <- tm_map(mycorpus, removePunctuation)
 mycorpus <- tm_map(mycorpus, removeNumbers)
+mycorpus <- tm_map(mycorpus, content_transformer(tolower))
 mycorpus <- tm_map(mycorpus, stripWhitespace)
 mycorpus  <- Corpus(VectorSource(mycorpus )) 
 #Document Tokenization#
@@ -59,9 +69,8 @@ tail(mycorpus)
 #Tokenize the corpus into n-grams:
 #We need to convert our cleaned mediaSample (cmediaSample) to corpus for
 #further exploration like ngrams(1,2,3) frequency distribution using packages tm and RWeka
-
 #  library
-corp <-  tm_map(mycorpus,stemDocument)
+corp <-  tm_map(corp,stemDocument)
 
 #3 ngrams tokenization functions for unigrams,bigrams and trigrams
 #Set the default number of threds to use, it is needed for ngram function.
@@ -78,20 +87,17 @@ dtm <- DocumentTermMatrix(mycorpus, control=list(tokenize=uniTokenizer))
 unifreq <- colSums(as.matrix(dtm))
 barplot(tail(sort(unifreq),20),las=2,main="Top 20 unigrams",cex.main=1,cex.axis =0.75,horiz=TRUE)
 
-
 #Top 20 most frequent bigrams
 dtm <- DocumentTermMatrix(mycorpus,control=list(tokenize=biTokenizer))
 bifreq <- colSums(as.matrix(dtm))
 barplot(tail(sort(bifreq),20),las=2,main="Top 20 bigrams",cex.main=1,cex.axis
         =0.75,horiz=TRUE)
 
-
 #Top 20 most frequent trigrams
 dtm <- DocumentTermMatrix(mycorpus,control=list(tokenize=triTokenizer))
 trifreq <- colSums(as.matrix(dtm))
 barplot(tail(sort(trifreq),20),las=2,main="Top 20 trigrams",cex.main=1,cex.axis
         =0.75,horiz=TRUE)
-
 
 #Top 20 most frequent quadrigrams
 dtm <- DocumentTermMatrix(mycorpus,control=list(tokenize=triTokenizer))
@@ -101,11 +107,5 @@ barplot(tail(sort(quadfreq),20),las=2,main="Top 20 quadrigrams",cex.main=1,cex.a
 #Word Cloud 
 library(wordcloud)
 freq.onegram <- sort(rowSums(as.matrix(unifreq)), decreasing = FALSE)
-wordcloud(names(freq.onegram), freq.onegram, max.words = 300, colors = brewer.pal(6, "Dark2"))
-
-freq.bigram <- sort(rowSums(as.matrix(bifreq)), decreasing = FALSE)
-wordcloud(names(freq.onegram), freq.onegram, max.words = 300, colors = brewer.pal(6, "Dark2"))
-
-freq.trigram <- sort(rowSums(as.matrix(trifreq)), decreasing = FALSE)
 wordcloud(names(freq.onegram), freq.onegram, max.words = 300, colors = brewer.pal(6, "Dark2"))
 
