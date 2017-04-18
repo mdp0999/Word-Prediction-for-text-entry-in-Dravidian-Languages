f4e16262d9ee04acb193475b9ae8bd5ed2eede3c
M.Durga Prasad <mdp0999@gmail.com>
2017-04-18 13:29:10 +0530

    Update dtm.R

--- dtm.R
+++ dtm.R
@@ -34,7 +34,7 @@ summary(mycorpus)
 mycorpus <- tm_map(mycorpus, removePunctuation)
 mycorpus <- tm_map(mycorpus, removeNumbers)
 mycorpus <- tm_map(mycorpus, stripWhitespace)
-mycorpus  <- Corpus(VectorSource(mycorpus )) 
+
 #Document Tokenization##
 #mycorpus <- tm_map(mycorpus, PlainTextDocument)
 #preview the sample after cleaning
